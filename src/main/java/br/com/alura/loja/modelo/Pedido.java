757c891c2039b7d29db578f156cd8077ae7e8b3b
Lucas Henrique Silva Gomes <lucas.gomes@jobconvo.com>
2021-04-17 15:51:23 -0300

    New Models

---
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,63 @@
+package br.com.alura.loja.modelo;
+
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="pedidos")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal valorTotal;
+    private LocalDate data = LocalDate.now();
+
+
+    @ManyToOne
+    private Cliente cliente;
+
+    public Pedido() {
+    }
+
+    public Pedido(BigDecimal valorTotal, LocalDate data, Cliente cliente) {
+        this.valorTotal = valorTotal;
+        this.data = data;
+        this.cliente = cliente;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
