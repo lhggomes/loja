f40d910729cce4959770550336dc78761f69373e
Lucas Henrique Silva Gomes <lucas.gomes@jobconvo.com>
2021-04-17 21:10:19 -0300

    Usando Criteria

--- src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
@@ -3,6 +3,10 @@ package br.com.alura.loja.dao;
 import br.com.alura.loja.modelo.Produto;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
@@ -80,6 +84,32 @@ public class ProdutoDAO {
         }
 
         return query.getResultList();
+    }
+
+    public List<Produto> buscarPorParametrosComCriteria(String nome, BigDecimal preco, LocalDate dataCadastro){
+
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Produto> query = builder.createQuery(Produto.class);
+        Root<Produto> from = query.from(Produto.class);
+
+        Predicate filtros = builder.and();
+
+        if (nome != null & nome.trim().isEmpty()){
+            builder.and(filtros, builder.equal(from.get("nome"), nome));
+
+        }
+        if (preco != null){
+            builder.and(filtros, builder.equal(from.get("preco"), preco));
+        }
+
+        if(dataCadastro != null){
+            builder.and(filtros, builder.equal(from.get("dataCadastro"), dataCadastro));
+        }
+
+
+        query.where(filtros);
+        return em.createQuery(query).getResultList();
+
     }
 
 }
